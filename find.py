f5f60fba687b382f857a98ed1773fc981ae20b8f
henning.schroeder <henning.schroeder@d2be66f1-a726-0410-9991-ad692b747e87>
2007-01-23 01:12:12 +0000

    some examples

---
+++ find.py
@@ -0,0 +1,11 @@
+from activemodel import *
+
+class Post(Model): has_many("comments", orderby="id desc")
+class Comment(Model): belongs_to("posts")
+Model.establish_connection("mysql://root@localhost/test")
+
+
+#print Post.find([1,3], conditions=["id>0", ("id<%s", 10)])
+#print Post.find_by_subject("second test")
+print Post.find_by(id=(0,3))
+#print Post.find(Post.subject !="first post")
